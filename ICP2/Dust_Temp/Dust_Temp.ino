b6e172b94dd2d5bac0626c6b824e3e52560da49f
playisme <playisme1@gmail.com>
2020-06-21 13:30:49 -0500

    ICP2-3
    Uloading ICP2-3 code.

---
+++ ICP2/Dust_Temp/Dust_Temp.ino
@@ -0,0 +1,95 @@
+int pin = 8;
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 2000; 
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+
+/* How to use the DHT-22 sensor with Arduino uno
+   Temperature and humidity sensor
+*/
+
+//Libraries
+#include <DHT.h>
+
+//Constants
+#define DHTPIN 7     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature valu
+
+
+
+void setup() {
+  Serial.begin(9600);
+  dustsetup();
+  tempsetup();
+
+  
+
+}
+
+void loop() {
+  dustloop();
+  temploop();
+}
+ 
+
+  
+
+
+
+void dustsetup(){
+  pinMode(8,INPUT);
+  starttime = millis();
+  
+}
+
+
+void dustloop(){
+  duration = pulseIn(pin, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+  if ((millis()-starttime) >= sampletime_ms) //if the sampel time = = 30s
+  {
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; 
+    Serial.print("Concentration = ");
+    Serial.print(concentration);
+    Serial.println(" pcs/0.01cf");
+    Serial.println("\n");
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  }
+
+}
+
+
+
+void tempsetup()
+{
+  dht.begin();
+}
+
+
+
+
+void temploop()
+{
+    delay(2000);
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+
+}
